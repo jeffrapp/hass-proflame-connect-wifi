e375ba3c85ac4a9d1baf2726fdbff91f6ad62783
Jeff Rapp <jrapp@spreetail.com>
2026-02-15 10:39:22 -0600

    Updated WiFi signal strength sensor type

--- custom_components/proflame_connect_wifi/sensor.py
+++ custom_components/proflame_connect_wifi/sensor.py
@@ -1,6 +1,12 @@
-"""Provides light control for Proflame fireplaces."""
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+"""Provides sensor entities for Proflame fireplaces."""
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import SIGNAL_STRENGTH_DECIBELS_MILLIWATT
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -17,30 +23,43 @@ async def async_setup_entry(
     """Create sensors for Proflame fireplaces."""
     entry_id = config_entry.entry_id
     coordinator: ProflameDataCoordinator = hass.data[DOMAIN][entry_id][PROFLAME_COORDINATOR]
-    async_add_entities(x for x in [
-        ProflameSensor(coordinator, ApiAttrs.FREE_HEAP, 'mdi:code-block-tags'),
-        ProflameSensor(coordinator, ApiAttrs.MIN_FREE_HEAP, 'mdi:code-block-tags'),
-        ProflameSensor(coordinator, ApiAttrs.WIFI_SIGNAL_STR, 'mdi:wifi'),
+    async_add_entities([
+        ProflameSensor(coordinator, ApiAttrs.FREE_HEAP, icon='mdi:code-block-tags'),
+        ProflameSensor(coordinator, ApiAttrs.MIN_FREE_HEAP, icon='mdi:code-block-tags'),
+        ProflameSensor(
+            coordinator,
+            ApiAttrs.WIFI_SIGNAL_STR,
+            icon='mdi:wifi',
+            device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+            state_class=SensorStateClass.MEASUREMENT,
+            unit=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+        ),
     ])
 
 class ProflameSensor(ProflameEntity, SensorEntity):
-    """Creates a sensore for Proflame fireplaces."""
+    """Creates a sensor for Proflame fireplaces."""
 
     def __init__(
         self,
         coordinator: ProflameDataCoordinator,
         api_attr: ApiAttrs,
         icon: str = None,
+        device_class: SensorDeviceClass = None,
+        state_class: SensorStateClass = None,
+        unit: str = None,
     ) -> None:
         """Create new instance of the ProflameSensor class."""
         super().__init__(coordinator, SensorEntityDescription(
             icon=icon,
             key=api_attr,
             translation_key=api_attr,
+            device_class=device_class,
+            state_class=state_class,
+            native_unit_of_measurement=unit,
         ))
         self._api_attr = api_attr
 
     @property
-    def native_value(self) -> bool | None:
+    def native_value(self) -> int | None:
         """Return the state of the sensor."""
         return self._device.get_state(self._api_attr)
